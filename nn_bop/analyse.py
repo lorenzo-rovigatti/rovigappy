67aba0c7c85c626746004f527d90e1d850b03c46
Lorenzo Rovigatti <lorenzo.rovigatti@gmail.com>
2019-08-20 15:57:42 +0200

    Add the l-method to the analyse.py script

--- nn_bop/analyse.py
+++ nn_bop/analyse.py
@@ -5,6 +5,7 @@ import numpy as np
 import sys
 
 import autoencoder as ae
+from matplotlib.mlab import slopes
 
 COLORS = ["red", "green", "blue", "yellow", "cyan", "magenta", "orange", "violet", "brown", "pink", "black", "grey"]
 
@@ -124,6 +125,8 @@ print("The number of initial gaussians is %d" % Ng)
 results = {}
 results[Ng] = (np.copy(cluster_probabilities), entropy(cluster_probabilities))
 
+# REDUCE THE NUMBER OF CLUSTERS FROM Ng TO 1
+
 # we reduce the number of gaussians by using the method of Baudry et al (2010)
 while Ng > 1:
     clusters = list(range(Ng))
@@ -149,3 +152,68 @@ with open("baudry_%s" % input_filename, "w") as baudry_file:
         colors = list(map(lambda x: COLORS[np.argmax(x)], results[Ng][0]))
         print_cogli1_conf(syst, colors, "gauss_%d_%s" % (Ng, input_filename))
         
+# DETECT THE "BEST" CLUSTER ACCORDING TO THE L-METHOD OF SALVADOR AND CHAN (2004)
+
+class FitResult(object):
+    def __init__(self, slope, intercept, x):
+        self.slope = slope
+        self.intercept = intercept
+        self.y = slope * x + intercept
+
+def best_fit(data):
+    x = data[:,0]
+    y = data[:,1]
+    
+    slope, intercept = np.polyfit(x, y, 1)
+    
+    return FitResult(slope, intercept, x)
+
+def root_mean_squared_error(y1, y2):
+    return np.sqrt(np.sum((y1 - y2)**2.) / len(y1))
+
+data = []
+for k in sorted(results):
+    data.append([k, results[k][1]])
+data = np.array(data)
+N_partitions = data.shape[0]
+if N_partitions < 4:
+    print("There are fewer than 4 clusters (%d), the l-method cannot be applied" % N_partitions, file=sys.stderr)
+    exit(1)
+
+position = 2
+min_rmse = np.infty
+while N_partitions - position >= 2:
+    left = data[0:position]
+    right = data[position:]
+
+    left_fit = best_fit(left)
+    right_fit = best_fit(right)
+
+    left_rmse = root_mean_squared_error(left[:,1], left_fit.y)
+    right_rmse = root_mean_squared_error(right[:,1], right_fit.y)
+
+    total_rmse = position / N_partitions * left_rmse + (N_partitions - position) / N_partitions * right_rmse
+    print("l-method, N_clusters: %d, rmse: %lf" % (data[position,0], total_rmse), file=sys.stderr)
+    if total_rmse < min_rmse:
+        min_rmse = total_rmse
+        min_position = position
+        min_crossing = (right_fit.intercept - left_fit.intercept) / (left_fit.slope - right_fit.slope)
+    
+    position += 1
+
+best_cluster_number = round(min_crossing)
+print("The best number of clusters according to the l-method is %d (crossing position: %lf, rmse: %lf)" % (best_cluster_number, min_crossing, min_rmse), file=sys.stderr)
+
+# here we assume that the number of clusters always starts at 1
+best_result = results[int(best_cluster_number)]
+clusters = list(map(lambda x: np.argmax(x), best_result[0]))
+to_sort = np.c_[clusters, encoded_bops]
+sorted_data = to_sort[to_sort[:,0].argsort()]
+
+last_c = 0
+with open("bops_%s" % input_filename, "w") as bops_file:
+    for v in sorted_data:
+        if v[0] > 0 and v[0] != last_c:
+            print("\n", file=bops_file)
+            last_c = v[0]
+        print(" ".join([str(x) for x in v[1:]]), file=bops_file)
